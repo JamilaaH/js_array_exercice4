5d8a9739817d5421ef97ba618b667843c2aff89e
Jamila <jaja_mimi@hotmail.com>
2020-12-30 15:36:04 +0100

    tableau

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+let panier = ["pomme", "poire", "orange"];
+console.log(panier);
+
+panier.push("Jamila");
+console.log(panier);
+
+panier.splice(1, 1, "cerise")
+console.log(panier);
+
+panier.pop()
+console.log(panier);
